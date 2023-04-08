6922834ff174a12a3ea1b15dbc98c9ad3ebcf1fe
igor-barcelos <barcelosenge@gmail.com>
2023-04-08 11:00:59 +0200

    save

--- src/Components/WhatsAppButton/WhatsAppButton.js
+++ src/Components/WhatsAppButton/WhatsAppButton.js
@@ -4,7 +4,7 @@ const WhatsAppButton = () => {
     return(
         <div> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
-        <a href="https://wa.me/5519998988908" class="whatsapp_float" target="_blank" rel="noopener noreferrer" >
+        <a href="https://wa.me/16017887740" class="whatsapp_float" target="_blank" rel="noopener noreferrer" >
         <i class="fa fa-whatsapp whatsapp-icon"></i>
       </a>
       </div>
